c37d2e0f2eeb5ee38165ada753d8405681250e95
Waryax <Waryax@users.noreply.github.com>
2018-03-05 11:13:58 +0200

    Projektipohja generoitu libGDX:llä

---
+++ android/src/fi/tamk/lucidstudio/tiltbeat/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tamk.lucidstudio.tiltbeat;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tamk.lucidstudio.tiltbeat.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
---
+++ desktop/src/fi/tamk/lucidstudio/tiltbeat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tamk.lucidstudio.tiltbeat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tamk.lucidstudio.tiltbeat.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
