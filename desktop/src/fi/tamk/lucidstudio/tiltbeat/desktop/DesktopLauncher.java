e6b3731ae85bd3ae712169915152353b52a990bb
Waryax <Waryax@users.noreply.github.com>
2018-03-11 18:34:51 +0200

    vahinkokaksoispiste vaihdettu puolipisteeks

--- desktop/src/fi/tamk/lucidstudio/tiltbeat/desktop/DesktopLauncher.java
+++ desktop/src/fi/tamk/lucidstudio/tiltbeat/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Tilt Beat";
-		config.width = 1280:
+		config.width = 1280;
         config.height = 720;
 		new LwjglApplication(new GameMain(), config);
 	}
