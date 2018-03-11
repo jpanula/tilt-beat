0f6657d97d293294336fe213260f755b5bee6f78
Waryax <Waryax@users.noreply.github.com>
2018-03-11 18:34:15 +0200

    työpöytäikkunan koko laitettu 1280x720 ja nimi laitettu "Tilt Beat"

--- desktop/src/fi/tamk/lucidstudio/tiltbeat/desktop/DesktopLauncher.java
+++ desktop/src/fi/tamk/lucidstudio/tiltbeat/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import fi.tamk.lucidstudio.tiltbeat.GameMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Tilt Beat";
+		config.width = 1280:
+        config.height = 720;
 		new LwjglApplication(new GameMain(), config);
 	}
 }
