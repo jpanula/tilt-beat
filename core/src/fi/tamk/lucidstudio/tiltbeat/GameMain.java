356754e8dd6e4d300d2007b53eb564e9efc7cdb8
Waryax <Waryax@users.noreply.github.com>
2018-03-11 19:29:37 +0200

    implementoitu screenien käyttö ja tehty GameScreen itse gameplaytä varten

--- core/src/fi/tamk/lucidstudio/tiltbeat/GameMain.java
+++ core/src/fi/tamk/lucidstudio/tiltbeat/GameMain.java
@@ -1,33 +1,44 @@
 package fi.tamk.lucidstudio.tiltbeat;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameMain extends ApplicationAdapter {
+public class GameMain extends Game {
 	SpriteBatch batch;
-	Texture img;
-	
+	OrthographicCamera camera;
+	GameScreen gameScreen;
+
+	public SpriteBatch getBatch() {
+	    return batch;
+    }
+
+    public OrthographicCamera getCamera() {
+	    return camera;
+    }
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	    batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 16, 9);
+        gameScreen = new GameScreen(this);
+	    setScreen(gameScreen);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	    super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		gameScreen.dispose();
+
 	}
 }
---
+++ core/src/fi/tamk/lucidstudio/tiltbeat/GameScreen.java
@@ -0,0 +1,70 @@
+package fi.tamk.lucidstudio.tiltbeat;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Jaakko on 11.3.2018.
+ */
+
+public class GameScreen implements Screen {
+    GameMain host;
+    SpriteBatch batch;
+    OrthographicCamera camera;
+
+    Texture img;
+
+    public GameScreen(GameMain host) {
+        this.host = host;
+        batch = host.getBatch();
+        camera = host.getCamera();
+
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(img, 0, 0, 1, 1);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+
+    }
+}
